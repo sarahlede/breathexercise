929347bef87b521d89860ce7861860572e251e0c
Sarah Lede <sml19f@fsu.edu>
2022-10-18 16:30:03 -0400

    PinkTriangles

--- sketch.js
+++ sketch.js
@@ -1,7 +1,14 @@
 function setup() {
-  createCanvas(400, 400);
+  createCanvas(400,400); 
 }
 
 function draw() {
-  background(125);
+  background('pink');
+  triangle(30, 75, 58, 20, 86,75);
+  triangle(30, 90, 60, 200, 150, 75);
+  triangle(300, 100, 320,100, 310, 80);
+
+  // Set colors
+  fill('yellow');
+  stroke(127, 63, 120);
 }
