08f2da7bdcc1d33343c0499670f0ade0e4812dd5
Sarah Lede <sml19f@fsu.edu>
2022-10-26 13:40:00 -0400

    addedmovingcircleturnedwhite

--- sketch.js
+++ sketch.js
@@ -1,5 +1,5 @@
-let canvasWidth = 400; // same variable as w
-let canvasHeight = 400; //same variable as h
+let canvasWidth = 1920; // same variable as w
+let canvasHeight = 1080; //same variable as h
 let x = 0;
 
 function setup() {
@@ -10,34 +10,62 @@ function setup() {
 }
 
 function draw() {
-  let r = random(100,200); 
-  let g = random(40);
-  let b = random(200,255); 
-  background( r, g, b ); //gives random a range of diff. colors
-  // background( 'pink' );
-  // let n = 0;
-  // while (n < 4) (
-  //   triangle(frameCount*0.1, 75, 58, 20, 86,75); 
-  // )
-  triangle(frameCount*0.1, 75, 58, 20, 86,75); 
-  if (x < 50) {
-    x++;
-  } 
+  // let r = random(100,200); 
+  // let g = random(40);
+  // let b = random(200,255); 
+  // background( r, g, b ); //gives random a range of diff. colors
+  background( 'black' );
+  let n = 0;
+  while (n < 4) {
+    circle(frameCount*0.1, 75*n, 58, 20, 86,75); 
+    n++;
+  
+  // triangle(frameCount*0.1, 75, 58, 20, 86,75); 
+  circle(frameCount, 90, 60, 200, 150, 75);
+  circle(300, 100, 320,100, 310, 80);
+}
+    // ellipse(0, 50, 33, 33); 
 
-  triangle(frameCount, 90, 60, 200, 150, 75);
-  triangle(300, 100, 320,100, 310, 80);
+    // push(); 
+    // strokeWeight(10);
+    // fill(204, 153, 0);
+    // translate(50, 0);
+    // ellipse(0, 50, 33, 33);
+    // pop();
 
+    // ellipse(100, 50, 33, 33);
+
+    // ellipse(0, 50, 33, 33);
+
+    // push();
+    // strokeWeight(10);
+    // fill(204, 153, 0);
+    // ellipse(33, 50, 33, 33); 
+
+    // push();
+    // stroke(0, 102, 153);
+    // ellipse(66, 50, 33, 33);
+    // pop();
+    
+    // pop();
+
+    // ellipse(100, 50, 33, 33);
   // if (x < 50) {
   //   x++;
   // } 
   // pertains to let x = 0, which has our shape move and stop pixel at anything less than 200
   // background( random (125)); flashing background
   // Set colors
-  fill( 'yellow');
+  fill( 'white');
   stroke(127, 63, 120);
 }
 
 function keyPressed() {
   fill(random(255));
 } 
- 
+  //   circle(frameCount*0.1, 75*n, 58, 20, 86,75); 
+  //   n++;
+  
+  // // triangle(frameCount*0.1, 75, 58, 20, 86,75); 
+  // circle(frameCount, 90, 60, 200, 150, 75);
+  // circle(300, 100, 320,100, 310, 80);
